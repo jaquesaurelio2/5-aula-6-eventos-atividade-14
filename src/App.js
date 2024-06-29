59fcb927a06598ba37a591027c9c5358c5dcddfa
jaquesaurelio2 <jaquesaurelio1@gmail.com>
2024-06-29 16:35:01 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="container">
+      <img src="https://picsum.photos/640/360" height="400px" alt="camera" />
+      <button>clique</button>
+    </div>
+  );
+}
